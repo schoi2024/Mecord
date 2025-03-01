a68863b6ba5b6feb766312e2b25842c4b978d8e9
Sally Kim <nkim207@illinois.edu>
2025-02-28 23:16:23 -0600

    Added frontend js

--- frontend.js
+++ frontend.js
@@ -1,1 +1,8 @@
 // Script for website interaction
+
+document.getElementById("inputField").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent form submission (if inside a form)
+        window.location.href = "askincome.html"; // Change to your desired page
+    }
+});
