a34f3ccc4dc560f8919458647e8125dd72de810c
Sally Kim <nkim207@illinois.edu>
2025-03-01 19:03:03 -0600

    Edited askDuration.

--- frontend.js
+++ frontend.js
@@ -4,21 +4,29 @@ let itemName = '';
 let price = '';
 let income = '';
 
-// front.html: retrieve item name
-document.getElementById("inputName").addEventListener("input", function(event) {
-    event.preventDefault();
-    itemName = document.getElementById("inputName").value;
-
-});
-
 // typing in price
-document.getElementById("inputNumber").addEventListener("input", function() {
-    let value = this.value.replace(/\D/g, ""); 
-    this.value = (value !== "") ? "$ " + value : "";
-});
+const inputNumberElement = document.getElementById("inputNumber");
+if (inputNumberElement) {
+    inputNumberElement.addEventListener("input", function(event) {
+        event.preventDefault();
+        let value = this.value.replace(/\D/g, ""); 
+        this.value = (value !== "") ? "$ " + value : "";
+    });
+}
+
+// front.html: retrieve item name
+const inputNameElement = document.getElementById("inputName");
+if (inputNameElement) {
+    inputNameElement.addEventListener("input", function(event) {
+        event.preventDefault();
+        itemName = inputNameElement.value;
+    });
+}
 
 // front.html: retrieve price value
-document.getElementById("priceForm").addEventListener("submit", function(event) {
+const priceFormElement = document.getElementById("PriceForm");
+if (priceFormElement) {
+    priceFormElement.addEventListener("submit", function(event) {
     event.preventDefault();
     
     price = document.getElementById("inputNumber").value;
@@ -27,13 +35,28 @@ document.getElementById("priceForm").addEventListener("submit", function(event) 
     // window.location.href = "askincome.html?item=" + encodeURIComponent(itemName) + "&price=" + encodeURIComponent(price);
 
 });
+}
 
+// askDuration.html: retrieve itemName from front
+const displayNameElement = document.getElementById("displayName");
+if (displayNameElement) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const loadedItemName = urlParams.get("item");
 
-const urlParams = new URLSearchParams(window.location.search);
-const loadedItemName = urlParams.get("item");
-
-if (loadedItemName) {
+    if (loadedItemName) {
     document.getElementById("displayName").textContent = "For how many weeks are you willing to save up for " + loadedItemName + "?";
+    }
+}
+
+const weekFormElement = document.getElementById("weekForm");
+if (weekFormElement) {
+    weekFormElement.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        week = document.getElementById("inputWeek").value;
+    
+        window.location.href = "askIncome.html?";
+    });
 }
 
 // askIncome.html: retrieve income value
